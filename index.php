da77001b8cd1e54cf8101a7e4dd9703b5a72900f
SujataPareek <pareeksujata19@gmail.com>
2025-08-20 20:11:12 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    
+<title>FOOD ORDERING SYSTEM</title>
+	<style>
+		a {
+		  display: inline;
+		  font-size: 40px;
+		  font-family: 'Lato', sans-serif;
+		  text-align: center;
+		  width: 15%;
+		  height: 5%;
+		  padding: 1%;
+		  margin-top: 15%;
+		  margin-left: 23%;
+		  margin-right: 0.1%;
+		  float: left;
+		  border: 10px solid gray;
+		  border-radius: 10px;  
+		  background-color: black; 
+		  opacity:0.8;
+		}
+		
+		a{
+			color:white;
+			font-size:25px;
+		}
+
+		a:link {
+		  text-decoration: none;
+		 
+		}
+
+		a:hover {
+		  background-color:gray;
+		}
+
+		body{
+			background-image: url(image/back.jpg);
+		}
+		
+		div{
+			color:white;
+		}
+	</style>				
+</head>
+<body >
+
+<div>
+<hr>
+<h1 style="text-align:center; font-family: 'Lato', sans-serif; font-size: 60px">FOOD ORDERING SYSTEM</h1>
+<hr>
+</div>
+
+	<a href="staff">STAFF MANAGEMENT</a>
+	<a href="admin">ADMIN MANAGEMENT</a>
+</body>
+</html>
